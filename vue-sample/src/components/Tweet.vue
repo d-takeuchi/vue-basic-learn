0d2ed1461501414bc13e40087d2f9af261406a54
d-takeuchi <kuronk.kuro3@gmail.com>
2022-05-30 23:37:46 +0900

    props使用

--- vue-sample/src/components/Tweet.vue
+++ vue-sample/src/components/Tweet.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import TweetPostForm from "./TweetPostForm.vue";
+import TweetList from "./TweetList.vue";
+
 const tweets = ref([
   { id: 0, description: "Hello, world!" },
   { id: 1, description: "this is the second tweet" },
@@ -20,19 +23,11 @@ const deleteTweet = (id: number) => {
 <template>
   <div class="container">
     <h1>Tweeter</h1>
-    <div class="form-container">
-      <input v-model="inputtingDescription" />
-      <button class="save-button" @click="postTweet()">post</button>
-    </div>
+    <TweetPostForm />
     <div class="tweet-container">
       <p v-if="tweets.length <= 0">No tweets have been added</p>
       <ul v-else>
-        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
-          <span>{{ tweet.description }}</span>
-          <button @click="deleteTweet(tweet.id)" class="delete-button">
-            delete
-          </button>
-        </li>
+        <TweetList :tweets="tweets" />
       </ul>
     </div>
   </div>
@@ -54,46 +49,6 @@ const deleteTweet = (id: number) => {
   width: 60%;
   margin-bottom: 12px;
   border-radius: 4px;
-}
-
-.tweet-list {
-  list-style: none;
-  margin-bottom: 12px;
-  border-radius: 4px;
-  font-size: 12px;
-  display: flex;
-  justify-content: space-between;
-  background-color: rgb(204, 219, 233);
-  padding: 8px 20px;
-  width: 300px;
-}
-
-.save-button {
-  color: #fff;
-  font-weight: bold;
-  background-color: #68c9c9;
-  border-radius: 2px;
-  border: none;
-  width: 60px;
-  height: 22px;
-}
-
-.save-button:hover {
-  background-color: #37bdbd;
-}
-
-.delete-button {
-  color: #fff;
-  font-weight: bold;
-  background-color: #c99a68;
-  border-radius: 2px;
-  border: none;
-  width: 60px;
-  height: 22px;
-}
-
-.delete-button:hover {
-  background-color: #ac783f;
 }
 
 input {
